1ce7689adce82d5f09379650605d518ada401214
pfokker <pfokker>
2011-05-06 05:14:58 +0000

    bugfix: https on a non-standard port yielded the wrong port number

--- was/websiteatschool/program/install.php
+++ was/websiteatschool/program/install.php
@@ -27,7 +27,7 @@
  * @copyright Copyright (C) 2008-2011 Ingenieursbureau PSD/Peter Fokker
  * @license http://websiteatschool.eu/license.html GNU AGPLv3+Additional Terms
  * @package wasinstall
- * @version $Id: install.php,v 1.3 2011/05/02 16:21:19 pfokker Exp $
+ * @version $Id: install.php,v 1.4 2011/05/06 05:14:58 pfokker Exp $
  * @todo how prevent third party-access to install.php after initial install? .htaccess? !exists(../config.php)? 
  * @todo we should make sure that autosession is disabled in php.ini, otherwise was won't work
  * @todo we should make sure that register globals is off
@@ -2623,6 +2623,9 @@ class InstallWizard {
         //
         if ((isset($_SERVER['SERVER_NAME'])) && (!empty($_SERVER['SERVER_NAME']))) {
             $hostname = $_SERVER['SERVER_NAME'];
+            if (isset($_SERVER['SERVER_PORT'])) {
+                $portnumber = $_SERVER['SERVER_PORT'];
+            }
         } elseif ((isset($_SERVER['HTTP_HOST'])) && (!empty($_SERVER['HTTP_HOST']))) {
             list($hostname,$portnumber) = explode(':',$_SERVER['HTTP_HOST']);
         } else {
